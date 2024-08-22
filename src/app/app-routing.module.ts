42d37a989748879c2303f8ef58122c29a0f1c6fd
gamerAK101 <perceptivelifes@gmai.com>
2024-08-22 13:19:46 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingScreenComponent } from './landing-screen/landing-screen.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component : LandingScreenComponent,
+    children:[
+      {
+        path:'',
+        component : DashboardComponent
+      },
+      {
+        path : 'appointment',
+        component :  AppointmentComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingScreenComponent } from './landing-screen/landing-screen.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopBarComponent,
+    NavBarComponent,
+    LandingScreenComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
